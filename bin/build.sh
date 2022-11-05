1faedaae2a4587466682058e0cd03b2b06a3775d
David Winter <i@djw.me>
2022-11-05 09:02:59 +0000

    ci: use render.com example for flask app

--- bin/build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -o errexit
-
-poetry --version
-poetry install
--- render_post_deploy/app.py
+++ render_post_deploy/app.py
@@ -5,6 +5,3 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="3000")
